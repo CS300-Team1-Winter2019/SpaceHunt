c4b00ab234c024609eed3b481f26de97183dd815
Max Smiley <max.jasper.smiley@gmail.com>
2019-02-14 14:50:06 -0800

    added another constructor for Ship using dylan's Settings object

--- Ship.js
+++ Ship.js
@@ -3,13 +3,31 @@ class Ship
     constructor(health)
     {
         this.health = health;
-        this.fuel = 0;
         this.sizeX =50;
         this.sizeY = 50;
         this.posX =125;//Math.floor(Math.random() * 127);
         this.posY = 125;//Math.floor(Math.random() * 127);
         this.delayMove = 500;
+
+        this.energy = 1000;
+        this.supplies = 100;
+        this.credits = 1000;
     }
+
+    constructor(health, settings)
+    {
+      this.health = health;
+      this.sizeX = 50;
+      this.sizeY = 50;
+      this.posX = 125;
+      this.posY = 125;
+      this.delayMove = 500;
+
+      this.energy = settings.startEnergy;
+      this.supplies = settings.startSupplies;
+      this.credits = settings.startCredits;
+    }
+
 
     damage(dmg)
     {
@@ -24,10 +42,10 @@ class Ship
     move(x, y)
     {
         if(x < 0) { x = 0; }
-        if(y < 0) { y = 0; }       
+        if(y < 0) { y = 0; }
         var until = new Date().getTime() + this.delayMove;
         while((new Date().getTime() < until)) {};
-        
+
         this.posX = x;
         this.posY = y;
     }
