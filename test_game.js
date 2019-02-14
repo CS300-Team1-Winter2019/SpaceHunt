7bb24aed98b3a42f6747f641cd4cedddd6acb7cd
gmillia <46548986+gmillia@users.noreply.github.com>
2019-02-14 14:02:11 -0800

    Add files via upload

--- test_game.js
+++ test_game.js
@@ -4,7 +4,6 @@ var ts = 608/32;
 var ship = new Ship(1000);
 var gameMap = new Map();
 document.addEventListener("keydown", move);
-document.addEventListener("click", shipMove)
 
 var objects =
 {
@@ -24,23 +23,8 @@ var objects =
     }
 };
 
-/*
-window.onload = function()
-{
-    ctx = document.getElementById('game').getContext("2d");
-    requestAnimationFrame(drawGame);
-    //drawGame();
-}
-*/
-
-<<<<<<< HEAD
-=======
 ctx = document.getElementById('game').getContext("2d");
-//requestAnimationFrame(drawGame);
 drawGame();
-//drawGame();
->>>>>>> 5ca8f099886fedb6ea4e883eeea66df5119e0e8e
-//shipMove();
 
 //currently not called
 function getUserInput(){
@@ -49,148 +33,22 @@ function getUserInput(){
   return {angle: angle, magnitude: magnitude};
 }
 
-<<<<<<< HEAD
-function movePause(){
-  var until = new Date().getTime() + 100;
-  while((new Date().getTime() < until)) {};
-}
-
-function shipMove(e)
-{
-  //e.preventDefault();
-  //should this function live inside ship or here in test_game?
-  //var userInput = getUserInput(); //this function should return an object {angle: ?, magnitude: ?}
-
-  //CHANGE THESE VALUES FOR TESTING
-  var userInput = {angle: 150, magnitude: 10};
-=======
-/*
-function shipMove()
-{
-    //should this function live inside ship or here in test_game?
-    //var userInput = getUserInput(); //this function should return an object {angle: ?, magnitude: ?}
-
-    //CHANGE THESE VALUES FOR TESTING
-    //var userInput = {angle: 150, magnitude: 10};
-    
-    var userInput = {angle: 120, magnitude: 60}
-
-    var runRise = ship.calculateXY(userInput); //this function returns an object {x: ?, y: ?}
-    var newPos = {x: ship.PosX+runRise.x, y: ship.PosY+runRise.y};
-
-    //accepting some messiness here for readability and cleanliness of other functions.
-    var run = runRise.x;
-    var rise = runRise.y;
-
-    console.log(newPos.x);
-    console.log(newPos.y);
-    console.log(run);
-    console.log(rise);
-    
-    while(ship.posX != newPos.x || ship.posY != newPos.y)
-    {
-        for(var i = 0; i > run; i--)
-        {
-            ship.posX = ship.posX - 1;
-            drawGame();
-        }
->>>>>>> 5ca8f099886fedb6ea4e883eeea66df5119e0e8e
-
-        for(var j = 0; j > rise; j--)
-        {
-            ship.posY = ship.posY - 1;
-            drawGame();
-        }
-    }
-}
-
-
-*/
-
 startMove();
 function startMove()
 {
     var gm = setInterval(function(){
-        shipMove(115, 115);
-    }, 100)
+        foo(gm, 115, 115);
+    }, 100);
 }
-function shipMove(newX, newY)
+
+var foo = function(gm, newX, newY)
 {
     if(ship.posX != newX || ship.posY != newY)
     {
         ship.move(ship.posX - 1, ship.posY -1);
-        drawGame();
-        //shipMove(newX, newY);   
+        drawGame(); 
     }
-}
-
-
-
-<<<<<<< HEAD
-//try uncommenting drawGame after you've run it. ship starts at original starting point again.
-    if(run != 0 && rise != 0){
-      var numTiles = riseAbs + runAbs;
-      for(var i = 0; i < numTiles; i+=2)
-      {
-          ship.PosY += yUnitVector;
-          drawGame();
-          movePause();
-          ship.PosX += xUnitVector;
-          drawGame();
-          movePause();
-
-          if(2*(newPos.x - ship.PosX) < (newPos.y - ship.PosY)){
-            ship.PosX += xUnitVector;
-            drawGame();
-            movePause();
-            i++;
-          }
-          else if(2*(newPos.y - ship.PosY) < (newPos.x - ship.PosX)){
-            ship.PosY += yUnitVector;
-            drawGame();
-            movePause();
-            i++;
-          }
-      }
-    }
-    else if(run == 0){
-      for(var i = 0; i < riseAbs; i++){
-        ship.PosY += yUnitVector;
-        drawGame();
-        movePause();
-      }
-    }
-    else if(rise == 0){
-      for(var i = 0; i < runAbs; i++){
-        ship.PosX += xUnitVector;
-        drawGame();
-        movePause();
-      }
-=======
-function Move(num)
-{
-    console.log(6);
-    switch(num)
-    {
-        case 37:
-            if(ship.PosX - 1 >= 0 && ship.PosX - 1 <= 127)
-                ship.PosX = ship.PosX-1;
-            break;
-        case 38:
-            if(ship.PosY - 1 >= 0 && ship.PosY - 1 <= 127)
-                ship.PosY = ship.PosY-1;
-            break;
-        case 39:
-            if(ship.PosX + 1 >= 0 && ship.PosX + 1 <= 127)
-                ship.PosX = ship.PosX+1;
-            break;
-        case 40:
-            if(ship.PosY + 1 >= 0 && ship.PosY + 1 <= 127)
-                ship.PosY = ship.PosY+1;
-            break;
->>>>>>> 5ca8f099886fedb6ea4e883eeea66df5119e0e8e
-    }
-    drawGame();
+    else clearInterval(gm);
 }
 
 function move(e)
@@ -202,7 +60,6 @@ function move(e)
         case 37:
             if(ship.PosX - 1 >= 0 && ship.PosX - 1 <= 127)
                 ship.PosX = ship.PosX-1;
-                drawGame();
             break;
         case 38:
             if(ship.PosY - 1 >= 0 && ship.PosY - 1 <= 127)
@@ -220,7 +77,7 @@ function move(e)
             shipMove();
             break;
     }
-    //drawGame();
+    drawGame();
 }
 
 function drawGame()
@@ -285,17 +142,4 @@ function drawGame()
             }
         }
     }
-<<<<<<< HEAD
-    window.requestAnimationFrame(drawGame);
-=======
-
-    //ship.move(ship.posX - 1, ship.posY - 1);
-    //requestAnimationFrame(drawGame);
-    //drawGame();
-
-    //shipMove();
->>>>>>> 5ca8f099886fedb6ea4e883eeea66df5119e0e8e
 }
-
-//let gm = setInterval(drawGame, 100);
-//shipMove();
