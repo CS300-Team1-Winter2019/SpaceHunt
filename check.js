a49f310bb49853bb5746e0cf502fa142a990ce12
jiacheng <jiacheng@pdx.edu>
2019-02-19 12:31:02 -0800

    change common to adapt all

--- check.js
+++ check.js
@@ -1,21 +1,21 @@
-<script>
+
 //M = prompt("Enter energy"); //using getenergy() function
 //N = prompt("Enter supplies"); //using getsupply() function
 
 function check_energy(a){
 	if(a<=0){
-		alert("Your energy is run out! LOL");
+		alert("Your energy is run out!");
 		return true;
 	}
 }
 function check_supply(a){
 	if(a<=0){
-		alert("Your supply is run out! LOL");
+		alert("Your supply is run out!");
 		return true;
 	}
 }
 //You guys can use check_supply_enerygy function anytime
-//just call: check_supply_enerygy(ship.energy,ship,supplies)
+//just call: check_supply_enerygy(gameVars.ship.energy,gameVars.ship.supplies)
 check_supply_energy(curr_Energy,curr_Supply){
 	//alert("check_supply_energy TESTING...")
 if(check_energy(curr_Energy) || check_supply(curr_Supply))
@@ -23,5 +23,3 @@ if(check_energy(curr_Energy) || check_supply(curr_Supply))
 else
 	alert("continue");
 }
-
-</script>
