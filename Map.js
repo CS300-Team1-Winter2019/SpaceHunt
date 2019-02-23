888c3acb6f870eb74f633270faed6d9e10d263ef
Kim Ma <pkm@pdx.edu>
2019-02-23 00:48:32 -0800

    Update Map.js
    Testing in process

--- Map.js
+++ Map.js
@@ -16,6 +16,8 @@ class Map
         this.maxPlanets = 200;
         this.maxWorms = 50;
         this.maxStations = 500;
+        // Dockable
+        this.maxDoc = 50;
 
         for(var i = 0; i < this.mapSize; i++)
         {
@@ -24,8 +26,10 @@ class Map
             {
                 //Random position at which something will be placed
                 var randPlacer = Math.floor(Math.random() * this.mapSize - 2);
+
                 //number of things e.g.: planets, holes, stations etc = # of colors
-                var maxChoices = 4;
+                var maxChoices = 5; // DOCKABLE changed from 4-5 to get one more choice for dock 
+                 
                 //Gives a starting range for random num: e.g: 2-4
                 var startAt = 0;
                 //Create new tile
@@ -43,12 +47,16 @@ class Map
                         if(this.maxWorms <= 0) { maxChoices--; startAt = 1; }
                         if(this.maxPlanets <= 0) { maxChoices--; startAt = 2; }
                         if(this.maxStations <= 0) { maxChoices--; startAt = 3; }
+                        if(this.maxDoc <= 0) { maxDoc--; startAt = 4}
 
                         var choice = Math.floor((Math.random() * maxChoices) + startAt);
 
                         if(choice == 0) { this.maxWorms--; }
                         if(choice == 1) { this.maxPlanets--; }
                         if(choice == 2) { this.maxStations--; }
+
+                        // DOCKABLE
+                        if(choice == 4) { this.maxDoc--;}
 
                         newTile.val = choice;
                         row.push(newTile);
